3ab90ae5a33bcf415563d5347498c513854d0769
Nursultan02 <nur.bek.aru.bag@gmail.com>
2021-03-02 18:45:02 +0600

    leaning bcrypt from background of the app and contain all of the code to bcrypt.js file

---
+++ bcryptTry.js
@@ -0,0 +1,19 @@
+const bcrypt = require('bcrypt');
+
+app.post('/user', (req, res) => {
+  bcrypt.hash(req.body.password, 10, (err, hash) => {
+    res.send(hash);
+  });
+
+  bcrypt.compare(
+    req.body.password,
+    '$2b$10$vp8ZOp0ZsIH409NcSiLoq.VAYQyt55FCxkRb2cSS9YP54qv8Vtky6',
+    (err, result) => {
+      if (result) {
+        res.send('Password is true, come in!');
+      } else {
+        res.send('Password is incorrect!');
+      }
+    }
+  );
+});
